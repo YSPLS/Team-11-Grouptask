47d540e552df8a3ad22da77cf114b42d7b63a189
IiroPeltonen <iiro23004@student.hamk.fi>
2024-02-08 13:41:17 +0200

    Nopea lisäys

--- ryhmatyo-mockup/php/list.php
+++ ryhmatyo-mockup/php/list.php
@@ -1,6 +1,6 @@
 <?php
 mysqli_report(MYSQLI_REPORT_ALL ^ MYSQLI_REPORT_INDEX);
-$yhteys=mysqli_connect("db", "asiakas", "password", "trtkp_11");
+$yhteys=mysqli_connect("db", "root", "password", "trtkp_11");
 $tulos=mysqli_query($yhteys, "select * from viesti");
 while ($rivi=mysqli_fetch_object($tulos)){
     print "<p> Nimi: $rivi->nimi, Viesti: $rivi->viesti ";
