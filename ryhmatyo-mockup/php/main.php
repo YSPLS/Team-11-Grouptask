e2352a7bc065c7d6ed4658b111cbcb8f60ceb628
YSPLS <lassi23004@student.hamk.fi>
2024-02-18 18:27:28 +0200

    aasdsd

--- ryhmatyo-mockup/php/main.php
+++ ryhmatyo-mockup/php/main.php
@@ -20,17 +20,15 @@ if (isset ($_POST["viesti"])){  //Jos viesti ruutuun on annettu tietoa se tallen
 else{
    exit;
 }
-
-?>
-
-<?php
-include ("../php/list.php"); //Tuodaan list.php
-?>
-
-<?php
 include ("../php/yhteys.php"); //Tuodaan yhteys.php
 
+$tulos=mysqli_query($yhteys, "select * from viesti"); //Määritetään "tulos" muuttuja, joka käyttää mysqli_query käytäntöä ja etsii tietokannasta kaiken viestit osiosta. Yhteys muuttujaa Viitataan jotta tiedämme mistä tietokannasta ja millä käyttäjällä ja millä salasanalla pääsemme sisään.
+while ($rivi=mysqli_fetch_object($tulos)){ //Tehdään loop jossa nimetään muuttuja "rivi" joka käyttää mysqli_fetch_object käytäntöä
+    print "<p> Nimi: $rivi->nimi, Viesti: $rivi->viesti "; //Etsitään taulukosta osiot nimi ja viesti jotka sitten tulostetaan niin pitkään kunnes kaikki on löydetty taulukosta
+}
+mysqli_close($yhteys); //Suljetaan mysqli yhteys
 ?>
+
 
 <?php
 include ("../html/footer.html"); //Tuodaan footer.html
