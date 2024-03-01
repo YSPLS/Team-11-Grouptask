389045263b121ce15d1759818f295c0669c6811c
Samii96 <sami.raty@student.hamk.fi>
2024-03-01 17:44:21 +0200

    muokkailua

--- ryhmatyo-mockup/php/poisto.php
+++ ryhmatyo-mockup/php/poisto.php
@@ -1,15 +1,15 @@
 <?php
-include ("../html/header.html"); //Tuodaan header
+include ("../html/header.html"); //Tuodaan header.html
 ?>
 <?php
-include ("../html/lomake.html"); //Tuodaan lomake
+include ("../html/lomake.html"); //Tuodaan lomake.html
 ?>
 
 <?php
+//Poistettava parametrin käsittelyä
 if (isset($_GET["poistettava"])) {
     $poistettava=$_GET["poistettava"];
 }
-
 
 $yhteys = mysqli_connect("db", "root", "password", "trtkp_11");
 
@@ -22,8 +22,10 @@ $tietokanta=mysqli_select_db($yhteys, "trtkp_11");
 if (!$tietokanta) {
     die("Tietokannan valinta epäonnistui: " . mysqli_connect_error());
 }
-echo "Tietokanta OK"; // debug
+echo "Tietokantayhteys OK"; // debug
 
+// Jos sivu ladataan niin että on annettu parametri 'poistettava', silloin
+// poistetaan kyseisellä id:llä oleva viesti tietokannasta
 if (isset($poistettava)) {
     $sql="delete from viesti where id=?";
     $stmt=mysqli_prepare($yhteys, $sql);
@@ -31,9 +33,10 @@ if (isset($poistettava)) {
     mysqli_stmt_execute($stmt);
 }
 
-
+// Muodostetaan lista tietokannassa olevista viesteistä (mahdollisen poiston jälkeen)
 $tulos=mysqli_query($yhteys, "SELECT * FROM viesti");
 
+// Tulostetaan muodostettu lista sivulle
 print "<ol>";
 while ($rivi=mysqli_fetch_object($tulos)) {
     print "<li>id=$rivi->id nimi=$rivi->nimi viesti=$rivi->viesti <a href='poisto.php?poistettava=".$rivi->id."'>Poista</a></li>";
@@ -42,5 +45,5 @@ print "</ol>";
 mysqli_close($yhteys);
 ?>
 <?php
-include ("../html/footer.html"); //Tuodaan footer.html
+include ("../html/footer_poisto.html"); //Tuodaan footer.html
 ?>
